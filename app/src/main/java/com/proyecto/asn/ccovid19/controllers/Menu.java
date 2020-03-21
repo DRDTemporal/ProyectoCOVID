18e548992dd0bcd5fc152451f3ae2a64f4ab42cd
DavidPyn27 <popayan159@gmail.com>
2020-03-21 17:39:50 -0500

    pregunta1

--- app/src/main/java/com/proyecto/asn/ccovid19/controllers/Menu.java
+++ app/src/main/java/com/proyecto/asn/ccovid19/controllers/Menu.java
@@ -3,14 +3,45 @@ package com.proyecto.asn.ccovid19.controllers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.proyecto.asn.ccovid19.R;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity implements View.OnClickListener {
+    Button btnSi, btnNo;
+    TextView txtRiesgo, txtUrgencias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        inicializar();
+    }
+
+    private void inicializar() {
+        btnSi = findViewById(R.id.btnSi);
+        btnNo = findViewById(R.id.btnNo);
+        btnSi.setOnClickListener(this);
+        btnNo.setOnClickListener(this);
+        txtRiesgo = findViewById(R.id.txtRiesgo);
+        txtUrgencias = findViewById(R.id.txtUrgencias);
+        txtRiesgo.setVisibility(View.INVISIBLE);
+        txtUrgencias.setVisibility(View.INVISIBLE);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnSi:
+
+                break;
+
+            case R.id.btnNo:
+
+                break;
+        }
     }
 }
