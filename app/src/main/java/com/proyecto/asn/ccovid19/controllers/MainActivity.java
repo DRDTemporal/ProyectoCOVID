b3e133a67a7f933884d454d9ff2a8c373e9dd818
RodrigoAGuspian <raguspian@misena.edu.co>
2020-03-21 15:54:26 -0500

    agregando clase persona

--- app/src/main/java/com/proyecto/asn/ccovid19/controllers/MainActivity.java
+++ app/src/main/java/com/proyecto/asn/ccovid19/controllers/MainActivity.java
@@ -8,10 +8,10 @@ import com.proyecto.asn.ccovid19.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
--- app/src/main/java/com/proyecto/asn/ccovid19/models/Persona.java
+++ app/src/main/java/com/proyecto/asn/ccovid19/models/Persona.java
@@ -1,4 +1,5 @@
 package com.proyecto.asn.ccovid19.models;
 
 public class Persona {
+    private String nombres, apellidos, tipoID, id,
 }
