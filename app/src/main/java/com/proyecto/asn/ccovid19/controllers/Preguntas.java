e029d8ecbf3525fdb36f2bdcc9fa78b2d1a6ef43
DavidPyn27 <popayan159@gmail.com>
2020-03-23 21:33:35 -0500

    imagnees

--- app/src/main/java/com/proyecto/asn/ccovid19/controllers/Preguntas.java
+++ app/src/main/java/com/proyecto/asn/ccovid19/controllers/Preguntas.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.proyecto.asn.ccovid19.R;
 
@@ -14,11 +15,13 @@ public class Preguntas extends AppCompatActivity implements View.OnClickListener
     LinearLayout primeraPregunta, SegundaPregunta,segundaPregunta2, TerceraPregunta, CuartaPregunta;
     Button btnSi, btnNo,btnSi2, btnNo2,btnSi21, btnNo21,btnSi3, btnNo3, btnSi4, btnNo4;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas);
         inicializar();
+
     }
 
     //METODO PARA INICIALIZAR VALORES
@@ -50,6 +53,7 @@ public class Preguntas extends AppCompatActivity implements View.OnClickListener
         btnSi4.setOnClickListener(this);
         btnNo4.setOnClickListener(this);
 
+
         //OCULTAR PREGUNTAS
         SegundaPregunta.setVisibility(View.INVISIBLE);
         segundaPregunta2.setVisibility(View.INVISIBLE);
@@ -63,6 +67,14 @@ public class Preguntas extends AppCompatActivity implements View.OnClickListener
         btnNo3.setVisibility(View.INVISIBLE);
         btnSi4.setVisibility(View.INVISIBLE);
         btnNo4.setVisibility(View.INVISIBLE);
+        btnSi2.setEnabled(false);
+        btnNo2.setEnabled(false);
+        btnSi21.setEnabled(false);
+        btnNo21.setEnabled(false);
+        btnSi3.setEnabled(false);
+        btnNo3.setEnabled(false);
+        btnSi4.setEnabled(false);
+        btnNo4.setEnabled(false);
     }
 
     @Override
@@ -73,18 +85,24 @@ public class Preguntas extends AppCompatActivity implements View.OnClickListener
             case R.id.btnSi:
                 Intent intent = new Intent(Preguntas.this, Resultados.class);
                 startActivity(intent);
+                finish();
                 break;
 
             case R.id.btnNo:
                 SegundaPregunta.setVisibility(View.VISIBLE);
                 btnSi2.setVisibility(View.VISIBLE);
                 btnNo2.setVisibility(View.VISIBLE);
+                btnSi2.setEnabled(true);
+                btnNo2.setEnabled(true);
+
                 break;
 
             case R.id.btnSi2:
                 segundaPregunta2.setVisibility(View.VISIBLE);
                 btnSi21.setVisibility(View.VISIBLE);
                 btnNo21.setVisibility(View.VISIBLE);
+                btnNo21.setEnabled(true);
+                btnNo21.setEnabled(true);
                 break;
 
 
@@ -92,40 +110,49 @@ public class Preguntas extends AppCompatActivity implements View.OnClickListener
                 TerceraPregunta.setVisibility(View.VISIBLE);
                 btnSi3.setVisibility(View.VISIBLE);
                 btnNo3.setVisibility(View.VISIBLE);
+                btnNo3.setEnabled(true);
+                btnNo3.setEnabled(true);
                 break;
 
             case R.id.btnSi21:
                 Intent intent2 = new Intent(Preguntas.this, Resultados.class);
                 startActivity(intent2);
+                finish();
                 break;
 
 
             case R.id.btnNo21:
                 Intent intent3 = new Intent(Preguntas.this, Resultados.class);
                 startActivity(intent3);
+                finish();
                 break;
 
 
             case R.id.btnSi3:
                 Intent intent4 = new Intent(Preguntas.this, Resultados.class);
                 startActivity(intent4);
+                finish();
                 break;
 
             case R.id.btnNo3:
                 CuartaPregunta.setVisibility(View.VISIBLE);
                 btnSi4.setVisibility(View.VISIBLE);
                 btnNo4.setVisibility(View.VISIBLE);
+                btnNo4.setEnabled(true);
+                btnNo4.setEnabled(true);
                 break;
 
             case R.id.btnSi4:
 
                 Intent intent5 = new Intent(Preguntas.this, Resultados.class);
                 startActivity(intent5);
+                finish();
                 break;
 
             case R.id.btnNo4:
                 Intent intent6 = new Intent(Preguntas.this, Resultados.class);
                 startActivity(intent6);
+                finish();
                 break;
 
 
