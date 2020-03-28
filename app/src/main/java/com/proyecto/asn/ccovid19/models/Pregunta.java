0d8664d6c4ce88545d748bd675cdfdbb759a3757
RodrigoAGuspian <raguspian@misena.edu.co>
2020-03-28 13:03:54 -0500

    agregando clase pregunta

---
+++ app/src/main/java/com/proyecto/asn/ccovid19/models/Pregunta.java
@@ -0,0 +1,30 @@
+package com.proyecto.asn.ccovid19.models;
+
+public class Pregunta {
+    private String pregunta;
+    private  boolean respuesta;
+
+    public Pregunta() {
+    }
+
+    public Pregunta(String pregunta, boolean respuesta) {
+        this.pregunta = pregunta;
+        this.respuesta = respuesta;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public boolean isRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(boolean respuesta) {
+        this.respuesta = respuesta;
+    }
+}
