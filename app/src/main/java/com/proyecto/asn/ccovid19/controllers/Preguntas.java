1345d1f386868b50714ed04878857595a4bdbcdf
RodrigoAGuspian <raguspian@misena.edu.co>
2020-03-28 20:39:59 -0500

    agregando GPS

--- app/src/main/java/com/proyecto/asn/ccovid19/controllers/Preguntas.java
+++ app/src/main/java/com/proyecto/asn/ccovid19/controllers/Preguntas.java
@@ -234,9 +234,9 @@ public class Preguntas extends AppCompatActivity implements View.OnClickListener
     private void guardarDatos() {
         try {
             mDatabase.child("persona").child(Objects.requireNonNull(mAuth.getUid())).child("caso").setValue(caso);
+            mDatabase.child("persona").child(Objects.requireNonNull(mAuth.getUid())).child("fechaDatos").setValue(horaFechaServidor);
             mDatabase.child("persona").child(Objects.requireNonNull(mAuth.getUid())).child("latitud").setValue(String.valueOf(location.getLatitude()));
             mDatabase.child("persona").child(Objects.requireNonNull(mAuth.getUid())).child("longitud").setValue(String.valueOf(location.getLongitude()));
-            mDatabase.child("persona").child(Objects.requireNonNull(mAuth.getUid())).child("fechaDatos").setValue(horaFechaServidor);
             if(solo1Vez==0){
                 solo1Vez=1;
                 startActivity(new Intent(Preguntas.this, Resultados.class));
